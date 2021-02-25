ba6ed6ba542f1ab57d306f44d3c7a33fb374320a
catsocks <3389801+catsocks@users.noreply.github.com>
2021-02-25 00:18:05 -0300

    Add build-website.py

---
+++ build-website.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import os
+import sys
+from pathlib import Path
+import shutil
+
+if __name__ == "__main__":
+    url = os.environ.get("URL", "")
+    if not url:
+        sys.exit("Must provide URL environment variable")
+
+    build = Path("build/wasm/release")
+    dest = Path("build/website")
+
+    os.makedirs(dest, exist_ok=True)
+
+    for name in ("index.html", "tennis.html", "tennis.js", "tennis.wasm"):
+        shutil.copyfile(build / name, dest / name)
+
+    for name in ("screenshot.png", "screenshot-2-by-1.png"):
+        shutil.copyfile(name, dest / name)
+
+    for file in ("index.html", "tennis.html"):
+        path = dest / file
+        text = path.read_text().replace(r"{{url}}", url)
+        path.write_text(text)
